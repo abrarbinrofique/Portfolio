e6665e85a06ce5c2e54db9f9064f5c9c38e46c79
abrarbinrofique <researchabrarrofique@gmail.com>
2024-11-04 22:12:44 +0600

    update portfolio without flex design

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+function scrollToSection() {
+    document.getElementById('target-section').scrollIntoView({ 
+        behavior: 'smooth' 
+    });
+}
+
+
+function scrollToSect() {
+    document.getElementById('target').scrollIntoView({ 
+        behavior: 'smooth' 
+    });
+}
+
+
+function scrolls() {
+    document.getElementById('project').scrollIntoView({ 
+        behavior: 'smooth' 
+    });
+}
